d1d4c6b6900a6aa29f19fae3fc32a1528489a951
Dax Wann <daxwann@gmail.com>
2019-06-23 19:55:06 -0700

    add sidebar

--- app/assets/javascripts/components/sidebar.js
+++ app/assets/javascripts/components/sidebar.js
@@ -1,23 +1,30 @@
 /* global $ */
 
 // dropdown function that applies or removes 'expand' class to the sections-sidebar nav
-const toggleSidebar = () => {
-	$("#sections-sidebar").toggleClass('expand');
+const openSidebar = (e) => {
+  e.stopPropagation();
+	$("#sections-sidebar").addClass('expand');
+  $("#sections-sidebar-btn").off('click', openSidebar);
+  $(document).on("click", closeSidebar);
 }
 
-const toggleSideBarIfExpanded = (e) => {
-	e.preventDefault();
-	if(e.currentTarget.className === 'sections-sidebar expand') {
-		setTimeout(toggleSidebar, 1000);
-	}
+const closeSidebar = () => {
+  $("#sections-sidebar").removeClass('expand');
+  $("#sections-sidebar-btn").on('click', openSidebar);
+  $(document).off("click", closeSidebar);
 }
 
 // Add click listener to hamburger icon which invokes sidebar function
-$(() => $('#sections-sidebar-btn').on('click', toggleSidebar));
-
+$(() => $('#sections-sidebar-btn').on('click', openSidebar));
 
 // Close an expanded sidebar after 1 second if mouse leaves sidebar element
 // Incomplete feature because sidebar does not remain open if mouse reenters element
-$(() => $('#sections-sidebar').mouseleave(toggleSideBarIfExpanded));
+let sidebarTimer;
 
+$(() => $('#sections-sidebar').mouseenter(() => {
+  clearTimeout(sidebarTimer);
+}));
 
+$(() => $('#sections-sidebar').mouseleave(() => {
+  sidebarTimer = setTimeout(closeSidebar, 1000); 
+}));
